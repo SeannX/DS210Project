36355d66fbe70eec76a7c30bfe310cf75f49f2cf
Xiao Nie <nx2004@bu.edu>
2024-12-10 15:26:05 -0500

    read csv lines added in main.rs. Tested with a println statement.

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,3 +1,9 @@
+mod data_loader;
+use data_loader::Edge;
+use data_loader::read_csv;
+
 fn main() {
-    println!("Hello, world!");
+    let csv_path = "soc-sign-bitcoinalpha.csv";
+    let graph: Vec<Edge> = read_csv(csv_path);
+    println!("{:?}", graph);
 }
