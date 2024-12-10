c9340b2a3f47e6c384b4e65bce23d900ce79121e
Xiao Nie <nx2004@bu.edu>
2024-12-10 16:13:41 -0500

    use graph::mk_graph to generate the Graph struct

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,9 +1,13 @@
+mod graph;
 mod data_loader;
-use data_loader::Edge;
 use data_loader::read_csv;
 
 fn main() {
     let csv_path = "soc-sign-bitcoinalpha.csv";
-    let graph: Vec<Edge> = read_csv(csv_path);
-    println!("{:?}", graph);
+    // The list of edges corresponds to the data
+    let edge_lst: Vec<graph::Edge> = read_csv(csv_path);
+
+    // Make a more convinient graph using the list of edges.
+    let graph: graph::Graph = graph::mk_graph(&edge_lst);
+    
 }
