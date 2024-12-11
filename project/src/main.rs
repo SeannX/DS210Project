12913bb5f3f3db6dfe24d4a251d23ae7e24507ad
Xiao Nie <nx2004@bu.edu>
2024-12-11 00:16:19 -0500

    Functions added in the main method to do the wrap-up calls

--- project/src/main.rs
+++ project/src/main.rs
@@ -1,6 +1,9 @@
 mod graph;
+use graph::Graph;
 mod data_loader;
 use data_loader::read_csv;
+mod analyze;
+use analyze::GraphInfo;
 
 fn main() {
     let csv_path = "soc-sign-bitcoinalpha.csv";
@@ -8,6 +11,11 @@ fn main() {
     let edge_lst: Vec<graph::Edge> = read_csv(csv_path);
 
     // Make a more convinient graph using the list of edges.
-    let graph: graph::Graph = graph::mk_graph(&edge_lst);
+    let graph: Graph = Graph::new(&edge_lst);
+
+    let graph_info: GraphInfo = GraphInfo::get_info(&graph);
     
+    println!("{:?}", graph_info.analyze_network_spread());
+
+    println!("{:?}", graph_info.analyze_trust_clustering());
 }
