85832a10493399b32be38909ebab215ff19f2e74
Xiao Nie <nx2004@bu.edu>
2024-12-11 00:15:36 -0500

    BFS moved to graph module, DFS not needed, so Algorithm module not needed anymore

--- project/src/graph/mod.rs
+++ project/src/graph/mod.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 
 // Edge struct used to represent a specific edge in the graph
 // the forth field - TimeStamp is removed since not using.
@@ -19,6 +20,7 @@ pub struct NodeNeighbors {
 // Grpah struct - corresponds to a complete graph
 // represents by a hashtable with the node as the key 
 // and the Edge struct containing the information of
+#[derive(Debug, Clone)]
 pub struct Graph { pub content: HashMap<usize, Vec<Edge>> }
 
 impl Graph {
@@ -31,11 +33,6 @@ impl Graph {
             graph_hashmap.entry(source_node).or_insert(Vec::new()).push(edge.clone());
         }
         return Graph{ content: graph_hashmap };
-    }
-
-    // simple getter method that fetch the related content of that ndoe.
-    pub fn get_edges(&self, node: usize) -> (usize, Vec<Edge>) {
-        return (node, self.content.get(&node).unwrap().clone());
     }
 
     // method that get the list of neighbors of a node.
@@ -61,6 +58,22 @@ impl Graph {
     }
 
 
+    // compute the indegree and outdegree of ALL nodes in the graph.
+    // return value: (indegree HashMap, outdegre HashMap)
+    pub fn get_degrees(&self) -> (HashMap<usize, usize>, HashMap<usize, usize>) {
+        let mut in_degree = HashMap::new();
+        let mut out_degree = HashMap::new();
+
+        for (node, edges) in &self.content {
+            out_degree.insert(*node, edges.len());
+
+            for edge in edges {
+                *in_degree.entry(edge.to).or_insert(0) += 1;
+            }
+        }
+
+        return (in_degree, out_degree);
+    }
 
     // This function calculates the clustering coefficient of a given node.
     // Clustering coefficient "is a measure of the degree to which nodes 
@@ -109,6 +122,80 @@ impl Graph {
         let possible_connections = num_nb * (num_nb - 1);
 
         return edges_btw_nb as f64 / possible_connections as f64
+    }
+
+    pub fn find_subgraphs(&self) -> Vec<Graph> {
+        let mut visited = HashSet::new();
+        let mut subgraphs = Vec::new();
+
+        for &node in self.content.keys() {
+            if !visited.contains(&node) {
+                // Use BFS to find all nodes in the connected subgraph
+                let subgraph_nodes = self.bfs(node);
+
+                // Construct the subgraph from the collected nodes
+                let mut subgraph_content = HashMap::new();
+                for node in subgraph_nodes {
+                    if let Some(edges) = self.content.get(&node) {
+                        subgraph_content.insert(node, edges.clone());
+                    }
+                    visited.insert(node);
+                }
+
+                subgraphs.push(Graph { content: subgraph_content });
+            }
+        }
+
+        return subgraphs;
+    }
+
+    // BFS algorithm that find all the connected node
+    // a helper function of find_subgraphs
+    pub fn bfs(&self, start_node: usize) -> HashSet<usize> {
+        let mut sub_graph_nodes = HashSet::new();
+        let mut queue = VecDeque::new();
+        let mut visited: HashSet<usize> = HashSet::new();
+
+        queue.push_back(start_node); 
+        sub_graph_nodes.insert(start_node);
+
+        while let Some(node) = queue.pop_front() {
+            sub_graph_nodes.insert(node);
+
+            if let Some(edges) = self.content.get(&node) {
+                for edge in edges {
+                    if !visited.contains(&edge.to) {
+                        visited.insert(edge.to);
+                        queue.push_back(edge.to);
+                    }
+                }
+            }
+        }
+        return sub_graph_nodes;
+    }
+
+    // Calculate the trust score of a given node
+    // this is determining by the sum of the
+    // weight of the indegree edges divided by
+    // the number of total indegrees.
+    pub fn get_trust_score(&self, node: usize) -> f64 {
+        let mut total_trust_score = 0.0;
+        let mut node_count = 0;
+
+        for (_, edges) in &self.content {
+            for edge in edges {
+                if edge.to == node {
+                    total_trust_score += edge.weight;
+                    node_count += 1;
+                }
+            }
+        }
+
+        if node_count == 0 {
+            return 0.0;
+        }
+
+        return total_trust_score as f64 / node_count as f64
     }
 
 }
